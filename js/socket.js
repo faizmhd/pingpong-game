c8cf152c33012d8be1e0df5d5d43500931988f16
Faizan <fmouhamad@hotmail.fr>
2020-04-11 13:36:05 +0200

    join two players on the same game

--- js/socket.js
+++ js/socket.js
@@ -1,5 +1,8 @@
 (function () {
-    var socket = io();
+    let pong;
+    let player
+    let socket = io();
+    // Creation of the Player one
     $('#new').on('click', () => {
         const name = $('#nameNew').val();
         if (!name) {
@@ -7,24 +10,69 @@
             return;
         }
         socket.emit('createGame', { name });
+        player = new Player(name, 'left');
     });
-
-    // New Game created by current client. Update the UI and create new Game var.
+    // Creation of the pong for P1
     socket.on('newGame', (data) => {
         const message =
             `Hello, ${data.name}. Please ask your friend to enter Game ID: 
       ${data.room}. Waiting for player 2...`;
 
-        // displayBoard(message);
-        $('.menu').css('display', 'none');
-        $('.main_game').css('display', 'block');
-        $('#userHello').html(message);
+        pong = new Game(data.room);
+        pong.displayBoard(message);
 
     });
-}());
 
-// displayBoard(message) {
-//     $('.menu').css('display', 'none');
-//     $('.main_game').css('display', 'block');
-//     $('#userHello').html(message);
-// }
+    $('#join').on('click', () => {
+        const name = $('#nameJoin').val();
+        const roomID = $('#room').val();
+        if (!name || !roomID) {
+            alert('Please enter your name and game ID.');
+            return;
+        }
+        socket.emit('joinGame', { name, room: roomID });
+        player = new Player(name, 'right');
+    });
+
+    socket.on('player1', (data) => {
+        const message = `Hello, ${player.getPlayerName()}`;
+        $('#userHello').html(message);
+    });
+
+    socket.on('player2', (data) => {
+        const message = `Hello, ${data.name}`;
+
+        pong = new Game(data.room);
+        pong.displayBoard(message);
+    });
+
+    socket.on('err', (data) => {
+        alert(data.message);
+        location.reload();
+    });
+
+    class Player {
+        constructor(name, position) {
+            this.name = name;
+            this.position = position;
+        }
+        getPlayerName() {
+            return this.name;
+        }
+        getPlayerPosition() {
+            return this.position;
+        }
+    }
+
+    class Game {
+        constructor(roomId) {
+            this.roomId = roomId;
+        }
+
+        displayBoard(message) {
+            $('.menu').css('display', 'none');
+            $('.main_game').css('display', 'block');
+            $('#userHello').html(message);
+        }
+    }
+}());
--- server.js
+++ server.js
@@ -18,6 +18,18 @@ io.on('connection', function (socket) {
         socket.join(`room-${++rooms}`);
         socket.emit('newGame', { name: data.name, room: `room-${rooms}` });
     })
+
+    // Connect the Player 2 to the room he requested. Show error if room full.
+    socket.on('joinGame', function (data) {
+        let room = io.nsps['/'].adapter.rooms[data.room];
+        if (room && room.length === 1) {
+            socket.join(data.room);
+            socket.broadcast.to(data.room).emit('player1', {});
+            socket.emit('player2', { name: data.name, room: data.room })
+        } else {
+            socket.emit('err', { message: 'Sorry, The room is full!' });
+        }
+    });
     socket.on('disconnect', function () {
         io.emit('user disconnected');
     });
