c33dcd91924a950ea16b31c7cb17d4d9927be966
Faizan <fmouhamad@hotmail.fr>
2020-04-13 22:07:10 +0200

    manage 4 players key and mouse events

--- js/game.control.js
+++ js/game.control.js
@@ -7,20 +7,52 @@ game.control = {
 
         game.control.controlSystem = "KEYBOARD";
 
-        if (game.amIPlayerOne) {
-            if (event.keyCode == game.keycode.KEYDOWN) {
-                game.playerOne.goDown = true;
-            } else if (event.keyCode == game.keycode.KEYUP) {
-                game.playerOne.goUp = true;
+        if(game.nb_players == 2){
+            if (game.playerId == 1) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerOne.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerOne.goUp = true;
+                }
+            }
+            else{
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerTwo.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerTwo.goUp = true;
+                }
             }
         }
-        else{
-            if (event.keyCode == game.keycode.KEYDOWN) {
-                game.playerTwo.goDown = true;
-            } else if (event.keyCode == game.keycode.KEYUP) {
-                game.playerTwo.goUp = true;
+        else if(game.nb_players == 4){
+            if (game.playerId == 1) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerOne.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerOne.goUp = true;
+                }
+            }
+            else if (game.playerId == 2) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerTwo.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerTwo.goUp = true;
+                }
+            } else if (game.playerId == 3) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerThree.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerThree.goUp = true;
+                }
+            } else if (game.playerId == 4){
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.PlayerFour.goDown = true;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.PlayerFour.goUp = true;
+                }
             }
         }
+
+        
 
         if (event.keyCode == game.keycode.SPACEBAR) {
             game.ball.start_game = true;
@@ -32,18 +64,48 @@ game.control = {
 
         game.control.controlSystem = "KEYBOARD";
 
-        if (game.amIPlayerOne) {
-            if (event.keyCode == game.keycode.KEYDOWN) {
-                game.playerOne.goDown = false;
-            } else if (event.keyCode == game.keycode.KEYUP) {
-                game.playerOne.goUp = false;
+        if(game.nb_players == 2){
+            if (game.playerId == 1) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerOne.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerOne.goUp = false;
+                }
+            }
+            else{
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerTwo.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerTwo.goUp = false;
+                }
             }
         }
-        else {
-            if (event.keyCode == game.keycode.KEYDOWN) {
-                game.playerTwo.goDown = false;
-            } else if (event.keyCode == game.keycode.KEYUP) {
-                game.playerTwo.goUp = false;
+        else if(game.nb_players == 4){
+            if (game.playerId == 1) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerOne.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerOne.goUp = false;
+                }
+            }
+            else if (game.playerId == 2) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerTwo.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerTwo.goUp = false;
+                }
+            } else if (game.playerId == 3) {
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerThree.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerThree.goUp = false;
+                }
+            } else if (game.playerId == 4){
+                if (event.keyCode == game.keycode.KEYDOWN) {
+                    game.playerFour.goDown = false;
+                } else if (event.keyCode == game.keycode.KEYUP) {
+                    game.playerFour.goUp = false;
+                }
             }
         }
     },
@@ -51,39 +113,109 @@ game.control = {
 
         game.control.controlSystem = "MOUSE";
 
-        if (!game.amIPlayerOne) {
+        if(game.nb_players == 2){
+            if (game.playerId == 2) {
 
-            if (event) {
-                game.control.mousePointer = event.clientY;
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+    
+                if (game.control.mousePointer > game.playerTwo.posY) {
+                    game.playerTwo.goDown = true;
+                    game.playerTwo.goUp = false;
+                } else if (game.control.mousePointer < game.playerTwo.posY) {
+                    game.playerTwo.goDown = false;
+                    game.playerTwo.goUp = true;
+                } else {
+                    game.playerTwo.goDown = false;
+                    game.playerTwo.goUp = false;
+                }
             }
-
-            if (game.control.mousePointer > game.playerTwo.posY) {
-                game.playerTwo.goDown = true;
-                game.playerTwo.goUp = false;
-            } else if (game.control.mousePointer < game.playerTwo.posY) {
-                game.playerTwo.goDown = false;
-                game.playerTwo.goUp = true;
-            } else {
-                game.playerTwo.goDown = false;
-                game.playerTwo.goUp = false;
+            else {
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+    
+                if (game.control.mousePointer > game.playerOne.posY) {
+                    game.playerOne.goDown = true;
+                    game.playerOne.goUp = false;
+                } else if (game.control.mousePointer < game.playerOne.posY) {
+                    game.playerOne.goDown = false;
+                    game.playerOne.goUp = true;
+                } else {
+                    game.playerOne.goDown = false;
+                    game.playerOne.goUp = false;
+                }
             }
         }
-        else {
-            if (event) {
-                game.control.mousePointer = event.clientY;
-            }
+        else if(game.nb_players == 4){
+            if (game.playerId == 2) {
 
-            if (game.control.mousePointer > game.playerOne.posY) {
-                game.playerOne.goDown = true;
-                game.playerOne.goUp = false;
-            } else if (game.control.mousePointer < game.playerOne.posY) {
-                game.playerOne.goDown = false;
-                game.playerOne.goUp = true;
-            } else {
-                game.playerOne.goDown = false;
-                game.playerOne.goUp = false;
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+            
+                if (game.control.mousePointer > game.playerTwo.posY) {
+                    game.playerTwo.goDown = true;
+                    game.playerTwo.goUp = false;
+                } else if (game.control.mousePointer < game.playerTwo.posY) {
+                    game.playerTwo.goDown = false;
+                    game.playerTwo.goUp = true;
+                } else {
+                    game.playerTwo.goDown = false;
+                    game.playerTwo.goUp = false;
+                }
+            }
+            else if (game.playerId == 3) {
+            
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+            
+                if (game.control.mousePointer > game.playerThree.posY) {
+                    game.playerThree.goDown = true;
+                    game.playerThree.goUp = false;
+                } else if (game.control.mousePointer < game.playerThree.posY) {
+                    game.playerThree.goDown = false;
+                    game.playerThree.goUp = true;
+                } else {
+                    game.playerThree.goDown = false;
+                    game.playerThree.goUp = false;
+                }
+            }else if (game.playerId == 4) {
+            
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+            
+                if (game.control.mousePointer > game.playerFour.posY) {
+                    game.playerFour.goDown = true;
+                    game.playerFour.goUp = false;
+                } else if (game.control.mousePointer < game.playerFour.posY) {
+                    game.playerFour.goDown = false;
+                    game.playerFour.goUp = true;
+                } else {
+                    game.playerFour.goDown = false;
+                    game.playerFour.goUp = false;
+                }
+            } else if (game.playerId == 1){
+                if (event) {
+                    game.control.mousePointer = event.clientY;
+                }
+            
+                if (game.control.mousePointer > game.playerOne.posY) {
+                    game.playerOne.goDown = true;
+                    game.playerOne.goUp = false;
+                } else if (game.control.mousePointer < game.playerOne.posY) {
+                    game.playerOne.goDown = false;
+                    game.playerOne.goUp = true;
+                } else {
+                    game.playerOne.goDown = false;
+                    game.playerOne.goUp = false;
+                }
             }
         }
+        
     },
     startTheGame: function () {
         game.ball.start_game = true;
